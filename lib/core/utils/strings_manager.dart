a14f6b424e1f499bbe405a45015c51066218dcba
Mina329 <minaemil329@gmail.com>
2023-08-25 16:35:18 +0300

    refactor

--- lib/core/utils/strings_manager.dart
+++ lib/core/utils/strings_manager.dart
@@ -1,6 +1,6 @@
 abstract class StringsManager{
   static const splashDescription = "online groceriet";
-  static const onBoardingDescription = "Welcome\nto our store";
+  static const onBoardingTitle = "Welcome\nto our store";
   static const getStarted = "Get Started";
-  
+  static const onBoardingDescription = "Ger your groceries in as fast as one hour";
 }
--- lib/features/onboarding/presentation/view/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/view/widgets/onboarding_view_body.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nectar/core/utils/assets_manager.dart';
@@ -11,40 +13,54 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        SvgPicture.asset(
-          AssetsManager.splashImg,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * .03,
-        ),
-        Text(
-          StringsManager.onBoardingDescription,
-          style: StylesManager.largeHeadingTextStyle,
-          textAlign: TextAlign.center,
-          maxLines: 2,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.07,
-        ),
-        Center(
-          child: SizedBox(
-            width: 350,
-            height: 70,
-            child: CustomElevatedBtn(
-              onPressed: () {},
-              txt: StringsManager.getStarted,
-              style: StylesManager.gilroySemiBold18,
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 3.5, sigmaY: 3.5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SvgPicture.asset(
+            AssetsManager.splashImg,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .03,
+          ),
+          Text(
+            StringsManager.onBoardingTitle,
+            style: StylesManager.largeHeadingTextStyle,
+            textAlign: TextAlign.center,
+            maxLines: 2,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.01,
+          ),
+          Text(
+            StringsManager.onBoardingDescription,
+            style: StylesManager.gilroyMedium16.copyWith(
+              color: const Color(0xffFCFCFC).withOpacity(0.7),
+            ),
+            textAlign: TextAlign.center,
+            maxLines: 2,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Center(
+            child: SizedBox(
+              width: 350,
+              height: 70,
+              child: CustomElevatedBtn(
+                onPressed: () {},
+                txt: StringsManager.getStarted,
+                style: StylesManager.gilroySemiBold18,
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.1,
-        )
-      ],
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          )
+        ],
+      ),
     );
   }
 }
