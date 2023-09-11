0f128084205cdde23c8bb71d289f48432936299e
Mina329 <minaemil329@gmail.com>
2023-09-11 23:58:48 +0300

    remove config.dart

--- lib/core/utils/config.dart
+++
@@ -1,5 +0,0 @@
-
-
-const String googleApiKey = "AIzaSyBjR0uzBx_yoMYwAJ4UDxJ_KmPSTALLCjc";
-const String geoCodingApiKey = "a769bae1e29a4c91bc307829444224ef";
-const String geoSearchAutoSuggestionApiKey = "eg1EVLE2jeAXV_58_QdLt4cBH9PIe5p5s__4MO_G2jM";
